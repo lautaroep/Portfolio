780c49a04e7120c177c005838b835a51841007d7
Lautaro <76933754+lautaroep@users.noreply.github.com>
2021-07-30 15:53:54 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+const main = {
+  name: 'Lautaro',
+  mail: 'Lautaroepastor@gmail.com',
+  img: 'img/user.gif',
+  role: ['Frontend Jr Developer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/lautaroep'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://linkedin.com/in/lautaropastor-'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'lautaroepastor@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Projects', link: './projects.html' },
+    { name: 'CV', link: 'https://drive.google.com/file/d/1anBdp37lexmhkrsPe_bwvkcAqtMv2Y9i/view?usp=sharing' },
+  ]
+};
